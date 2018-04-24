d51eb08599e9ca64ccd4500f016a701e26b221aa
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-24 14:35:22 -0700

    bunch of stuff

---
+++ test/unit/User.test.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/User');
+
+describe('User model', () => {
+
+    const data = {
+        email: 'me@me.com'
+    };
+
+    const password = 'abc';
+    let user = null;
+
+    beforeEach(() => {
+        user = new User(data);
+        user.generateHash(password);
+    });
+
+    it('generates has form password', () => {
+        assert.ok(user.hash);
+        assert.notEqual(user.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(user.comparePassword(password));
+    });
+});
